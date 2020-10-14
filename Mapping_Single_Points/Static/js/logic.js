ec586525912992f703c1ddb3412ff4523a6d5c94
Harishawn Ramrup <ramrupshawn@gmail.com>
2020-10-14 18:48:03 -0400

    added population markers

--- Mapping_Single_Points/Static/js/logic.js
+++ Mapping_Single_Points/Static/js/logic.js
@@ -6,12 +6,18 @@ console.log("working");
 let map = L.map("mapid").setView([40.7,-94.75], 4);
 
 
+// Get data from cities.js
+let cityData = cities;
 
-let marker = L.circleMarker([34.0522, -118.2437], {
-    radius: 100,
-    color: "black",
-    fillColor: "#ffffa1"
- }).addTo(map);
+  //loop through the cities
+  cityData.forEach(function(city){
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+  });
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
