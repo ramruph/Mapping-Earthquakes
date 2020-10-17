55992a82035114eebdd40c463e21514fd898728b
Harishawn Ramrup <ramrupshawn@gmail.com>
2020-10-17 13:40:43 -0400

    Polygon map

---
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -0,0 +1,39 @@
+//check if code is woirking
+console.log("working");
+// Create the map object with center and zoom level. This is the geographical center of the earth
+let map = L.map('mapid').setView([44, -80], 2);
+
+
+//JSON data
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/ramruph/Mapping-Earthquakes/main/torontoRoutes.json"
+
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
+
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data, {
+  style: myStyle,
+  onEachFeature: function(feature, layer){
+    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3><hr><h3> Destination: " + feature.properties.dst + "</h3>" );
+  }
+}).addTo(map);
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
